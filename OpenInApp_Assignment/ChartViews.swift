b0a48e0e3eaaa34fd650fb5d30521ff043c60e14
Smit Patel <smit24@icloud.com>
2024-04-07 15:03:23 +0530

    done!!

---
+++ OpenInApp_Assignment/ChartViews.swift
@@ -0,0 +1,8 @@
+//
+//  ChartViews.swift
+//  OpenInApp_Assignment
+//
+//  Created by Smit Patel on 07/04/24.
+//
+
+import Foundation
---
+++ OpenInApp_Assignment/HstackItemView.swift
@@ -0,0 +1,8 @@
+//
+//  HstackItemView.swift
+//  OpenInApp_Assignment
+//
+//  Created by Smit Patel on 06/04/24.
+//
+
+import Foundation
